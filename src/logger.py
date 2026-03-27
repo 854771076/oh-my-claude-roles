a52a1deebc03eb2529e6e9986d0ddabcdd06dad3
fuyang <854771076@qq.com>
2026-03-27 14:29:46 +0800

    feat: add logger configuration with Loguru

---
+++ src/logger.py
@@ -0,0 +1,27 @@
+from loguru import logger
+import sys
+
+
+def setup_logger(verbose: bool = False):
+    """Setup logger with appropriate verbosity"""
+    logger.remove()
+    level = "DEBUG" if verbose else "INFO"
+
+    # Console output
+    logger.add(
+        sys.stderr,
+        level=level,
+        format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>",
+    )
+
+    # File output with rotation
+    logger.add(
+        "logs/oh-roles.log",
+        level="DEBUG",
+        rotation="10 MB",
+        retention="7 days",
+        encoding="utf-8",
+    )
+
+
+__all__ = ["logger", "setup_logger"]
